720055a507151251fe444b376c76aa2537f110d6
Amari Chanell <amarichanell@Amaris-MacBook-Air.local>
2026-04-28 11:27:14 -0700

    remove secret injection from base branch

--- secret-injection/secure_check.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-result = subprocess.run(
-    ["/usr/bin/security", "find-generic-password", "-s", "my_app_service", "-a", "my_username", "-w"],
-    capture_output=True,
-    text=True
-)
-
-print(result.stdout)
